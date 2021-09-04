e5ed06e14d00a3a43b52091c21464f7640742e6c
juandv30 <78824982+juandv30@users.noreply.github.com>
2021-09-04 17:38:49 -0500

    Add Entity

---
+++ todoentry/todoentry.Common/Models/Company.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace todoentry.Common.Models
+{
+    public class Company
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
